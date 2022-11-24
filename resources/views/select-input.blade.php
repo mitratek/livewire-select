adeee163c84b55ef819b0eec62e083cedc3b5f0d
Ardian Riftha Dhuha <ardian@mitratek.com>
2022-11-24 07:56:17 +0700

    Fixed loading indicator not show up when loading

--- resources/views/select-input.blade.php
+++ resources/views/select-input.blade.php
@@ -1,15 +1,17 @@
 <div x-data="{ dropdownOpen: false }" class="relative">
     <input @click="dropdownOpen = ! dropdownOpen" @click.away="dropdownOpen = false" type="text" wire:model.debounce.1000ms="queryData" wire:click="getQueryData" wire:keydown.tab="getQueryData" class="w-full shadow appearance-none border rounded py-2 pl-3 pr-8 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="{{ $placeholder }}" autocomplete="mitracloud-off" />
 
-    <span wire:loading wire:target="queryData" class="absolute inset-y-2 right-0 flex items-center pr-2">
-        <x-loading-indicator />
-    </span>
+    <div class="absolute inset-y-2 right-0 flex items-center pr-2">
+        <div wire:loading>
+            <x-loading-indicator />
+        </div>
+    </div>
 
-    <span wire:loading.remove class="absolute inset-y-2 right-0 flex items-center pr-2">
+    <div wire:loading.remove class="absolute inset-y-2 right-0 flex items-center pr-2">
         <div wire:click="clearValue()">
             <x-clear-button />
         </div>
-    </span>
+    </div>
 
     @if(isset($message))
     <div class="absolute z-10 max-h-20 overflow-y-auto w-full bg-gray-50 rounded-t-none rounded-b-lg shadow-lg list-group">
